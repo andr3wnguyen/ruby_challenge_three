45bac2a63d0db67da6f842be310246dd6ca54b99
Andrew <andynguyen93@hotmail.co.uk>
2022-10-31 15:48:39 +0000

    ready to be reviewed

--- chapter2/challenges/program/lib/password_manager.rb
+++ chapter2/challenges/program/lib/password_manager.rb
@@ -1,2 +1,19 @@
 class PasswordManager
+    def initialize
+        @hash = {}
+    end
+
+    def add(service, password)
+        if password.length > 7 && password.match?(/[!@£$&%]/)
+            @hash[service] = password
+        end
+    end
+
+    def password_for(service)
+        return @hash[service]
+    end
+
+    def services
+        return @hash.keys
+    end
 end
