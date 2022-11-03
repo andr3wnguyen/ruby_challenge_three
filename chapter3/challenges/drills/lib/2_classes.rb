707b331c348f016398169d8cf64d761822a28c7a
Andrew <andynguyen93@hotmail.co.uk>
2022-11-03 18:56:58 +0000

    ready for review

--- chapter3/challenges/drills/lib/2_classes.rb
+++ chapter3/challenges/drills/lib/2_classes.rb
@@ -23,6 +23,7 @@
 # Takes one string as an arg (a name)
 # Returns 'Hello, Rita', if the arg is 'Rita'
 
+
 # Example solution
 
 # > class Greeter
@@ -45,12 +46,40 @@
 # Takes one string as an arg (a name)
 # Returns 'Goodmorning, Alex', if the arg is 'Alex'
 
+class Greeter
+  def hello(name)
+    return "Hello, #{name}"
+  end
+  def goodbye(name)
+    return "Goodbye, #{name}"
+  end
+  def goodnight(name)
+    return "Goodnight, #{name}"
+  end
+  def goodmorning(name)
+    return "Goodmorning, #{name}"
+  end
+end
+
+
 # Basket
 # add
 # takes one argument of any type
 # adds it to the list of items
 # items
 # returns everything that has been added to the basket
+class Basket
+  def initialize
+    @bag = []
+  end
+  def add(arg)
+    @bag << arg
+  end
+  def items
+    return @bag
+  end
+end
+
 
 # Basket2
 # add
@@ -60,6 +89,17 @@
 # returns everything that has been added to the basket
 # eg: after having added "carrots", 3
 # contents returns [{"item" => "carrots", "amount" => 3}]
+class Basket2
+  def initialize
+    @contents = []
+  end
+  def add(item, amt)
+    @contents << {"item"=>item, "amount"=>amt}
+  end
+  def contents
+    return @contents
+  end
+end
 
 # Calculator
 # add
@@ -83,6 +123,33 @@
 # eg: after multiplying 4 and 6, the history is 
 # [{"operation" => "multiply", "arguments" => [4, 6], "result" => 24}]
 
+class Calculator
+  def initialize
+    @history = []
+  end
+  def add(a,b)
+    @history << {"operation" => "add", "arguments" => [a,b], "result" => a+b}
+    return a+b
+  end
+  def multiply(a,b)
+    @history << {"operation" => "multiply", "arguments" => [a,b], "result" => a*b}
+    return a*b
+  end
+  def divide(a,b)
+    @history << {"operation" => "divide", "arguments" => [a,b], "result" => a/b}
+    return a/b
+  end
+  def subtract(a,b)
+    @history << {"operation" => "subtract", "arguments" => [a,b], "result" => a-b}
+    return a-b
+  end
+  def history
+    return @history
+  end
+end
+
+
+
 # Cohort
 # add_student
 # takes one hash, representing a student, as an arg.
@@ -94,9 +161,28 @@
 # takes one string, the name of an employer, as an arg.
 # E.g. 'NASA'
 # returns only the students who work for that employer
+class Cohort
+  def initialize
+    @studentslist =[]
+  end
+  def add_student(hash)
+    @studentslist << hash
+  end
+  def students
+    return @studentslist
+  end
+  def employed_by(employ)
+    @studentslist.select {|hashdata| hashdata["employer"] == employ}
+  end
+end
+
+
+
+
 
 # Person
-# is initialized with a complex hash, for example...
+# is initialized with a complex hash, for example...\
+"""
 {
   'name' => 'alex',
   'pets' => [
@@ -108,7 +194,18 @@
     {'name' => 'work', 'building' => '50', 'street' => 'Commercial Street'},
     {'name' => 'home', 'building' => '10', 'street' => 'South Street'}
   ]
-}
+}"""
+
+#get address info
+#building = @alex.select {|k,v| k == "addresses" }.values.flatten.select {|key,val| p val["name"] == "work"}[0]["building"]
+#street = @alex.select {|k,v| k == "addresses" }.values.flatten.select {|key,val| p val["name"] == "work"}[0]["street"]
+
+#gets pets
+#petname= @alex.select {|key,val| key == 'pets'}.values.flatten[0]["name"]
+#petanimal = @alex.select {|key,val| key == 'pets'}.values.flatten[0]["animal"]
+
+#petssize
+# petsize = @alex.select {|key,val| key == "pets"}.values.flatten.size - alex has petsize pets
 
 # work_address
 # takes no args
@@ -121,10 +218,36 @@
 # pets
 # takes no args
 # returns a nice summary of the person's pets
-
 # E.g.
 # Alex has 3 pets
 # - a cat called Arthur
 # - a dog called Judith
 # - a goldfish called Gwen
-#
+
+class Person
+  def initialize(person)
+    @person = person
+  end
+  def work_address
+    address = ""
+    building = @person.select {|k,v| k == "addresses" }.values.flatten.select {|val| val["name"] == "work"}[0]["building"]
+    street = @person.select {|k,v| k == "addresses" }.values.flatten.select {|val| val["name"] == "work"}[0]["street"]
+    address += building + " " + street    
+    return address
+  end
+
+  def home_address
+    address = ""
+    building = @person.select {|k,v| k == "addresses" }.values.flatten.select {|val| val["name"] == "home"}[0]["building"]
+    street = @person.select {|k,v| k == "addresses" }.values.flatten.select {|val| val["name"] == "home"}[0]["street"]
+    address += building + " " + street    
+    return address
+  end
+
+  def pets
+    owner = @person.select {|k,v| k == "name"}.values.join()
+    petsize = @person.select {|key,val| key == "pets"}.values.flatten.size.to_s
+    pets = @person.select {|key,val| key == "pets"}.values.flatten.map {|x| "- a "+ x["animal"] + " called " + x["name"]+ "\n"}
+    return owner + " has " + petsize + " pets\n" + pets.join()
+  end
+end
--- chapter3/challenges/program/lib/password_manager_2.rb
+++ chapter3/challenges/program/lib/password_manager_2.rb
@@ -1,2 +1,85 @@
 class PasswordManager2
+    #initiate using an empty list, contains hashemap hash containing a hash set of system:pass and dateadded:date
+    #e.g. [{service:x, password:y date_added:z}, etc]
+    def initialize
+        @pwm = []
+    end
+    
+    #stores service and password
+    #unless password is not unique -> password_exists?
+    #or service already has a stored password 
+    def add(service,password)
+        if !password_exists?(password)
+            if !service_exists?(service)
+                @pwm << {service: service,
+                        password: password,
+                        added_on: Time.now}
+            else
+                return "ERROR: Service names must be unique"
+            end
+        else
+            return "ERROR: Passwords must be unique"
+        end
+    end
+
+    #deletes service and password
+    def remove(service)
+        @pwm.delete(@pwm.find {|serv| serv[:service]==service})
+    end
+
+    #lists services which has a password -> password_exists?
+    def services
+        @pwm.map {|serv| serv[:service]}
+    end
+
+    #sorts by list and returns services sorted as such
+    def sort_by(list)
+        if list == "service"
+            services().sort
+        elsif list == "added_on"
+            return @pwm.sort_by! {|date| date[:date_added]}.reverse.map {|x| x[:service]}
+        else 
+            return "please enter service or added_on"
+        end
+    end
+
+    #returns password for service
+    def password_for(service)
+        @pwm.find {|serv| serv[:service]==service}[:password]
+    end
+    
+    #updates password 
+    #unless password already exists -> password_exists?
+    def update(service, new_password)
+        if !password_exists?(new_password)
+            @pwm.find {|serv| serv[:service]==service}[:password] = new_password
+        else
+            return "ERROR: not updated password is not unique"
+        end
+    end
+
+    #check is password already exists within the list and returns true if so
+    def password_exists?(password)
+        if @pwm.empty?
+            return false 
+        elsif @pwm.map {|pass| pass[:password]}.include?(password)
+            return true
+        else
+            return false
+        end
+    end
+
+        #checks to see if service exists within the list and returns true if so
+    def service_exists?(service)
+        if @pwm.empty?
+            return false
+        #checks if :service => service is within the pwm
+        elsif @pwm.find {|serv| serv[:service]}[:service] == service
+            return true
+        else
+            return false
+        end
+    end
+
 end
+
